246430f9f2a1e7ea774de1e71476f8377d4e46a0
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-07-03 11:43:08 +0530

    intial commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import styled from 'styled-components';
+
+const Layout = () => {
+  return <div>Layout</div>;
+};
+
+export default Layout;
